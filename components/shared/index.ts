85537786b41228bbe99b209131e63e12f6bcc0f9
Ales-Bas <baskakov.a.a@yandex.ru>
2024-08-20 20:15:19 +0300

    init

---
+++ components/shared/index.ts
@@ -0,0 +1,10 @@
+export { Container } from './container';
+export { Header } from './app-header';
+export { Title } from './title';
+export { Categories } from './catigories';
+export { SortPopup } from './sort-popup';
+export { TopBar } from './top-bar';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export { RangeSlider } from './range-slider';
+export { ProductsGroupList } from './products-group-list';
